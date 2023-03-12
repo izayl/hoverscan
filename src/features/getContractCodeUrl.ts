10d4d2530e28fe314cf9b49a19830be536edd8a6
izayl <izayl@163.com>
2023-03-12 10:50:24 +0800

    refactor: reorg fold

--- src/features/getContractCodeUrl.ts
+++
@@ -1,9 +0,0 @@
-import { ALL_SUPPORTED_EXPLORERS } from "~src/constants/explorers"
-
-export const getContractCodeUrl = (address: string, chainId: number) => {
-  const explorer = ALL_SUPPORTED_EXPLORERS[chainId]
-
-  if (!explorer) return null
-
-  return `${explorer.url.replace(/\.\w+(\/)/, ".deth.net/")}/address/${address}`
-}
--- src/features/isContract.ts
+++
@@ -1,6 +0,0 @@
-import type { Provider } from "ethers"
-
-export const isContract = async (address: string, provider: Provider) => {
-  const code = await provider.getCode(address)
-  return code !== "0x"
-}
