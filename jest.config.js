2f9c8d817c1c4950eb574b7c5374990cd7215802
izayl <izayl@163.com>
2023-03-23 00:07:09 +0800

    ci: project deps setup

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
+  preset: 'ts-jest',
+  testEnvironment: 'js-dom',
   moduleNameMapper: {
-    "^~src/(.*)$": "<rootDir>/src/$1"
+    '^~lib/(.*)$': '<rootDir>/lib/$1',
   },
-  setupFiles: ["dotenv/config"]
+  setupFiles: ['dotenv/config'],
 }
