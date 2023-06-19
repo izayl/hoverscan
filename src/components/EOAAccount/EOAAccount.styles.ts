db7ba6aea08a9253a8f3353d7db318bc941b24d6
izayl <izayl@163.com>
2023-06-19 08:11:06 +0800

    fix: react import

--- src/components/EOAAccount/EOAAccount.styles.ts
+++ src/components/EOAAccount/EOAAccount.styles.ts
@@ -4,6 +4,6 @@ export const eoaSlots = tv({
   slots: {
     resolvedName: 'rounded-xl py-0.5 px-2 font-medium text-sm bg-gray-100 cursor-pointer flex items-center gap-1',
     lens: 'bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-pink-500',
-    addressText: 'text-xs font-medium text-[#444] wrap-anywhere',
+    addressText: 'text-xs font-medium text-[#444] break-all',
   },
 })
--- src/components/SyncStatus/SyncButton.tsx
+++ src/components/SyncStatus/SyncButton.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { type VariantProps, tv } from 'tailwind-variants'
 
 const syncButton = tv({
