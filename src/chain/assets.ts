d319f9fc4d3b34358c87d33ff8785188c669c85c
izayl <izayl@163.com>
2023-10-14 01:03:54 +0800

    feat: add assets

---
+++ src/chain/assets.ts
@@ -0,0 +1,24 @@
+import * as chains from './chains'
+
+export const icons = {
+  default: require('/assets/icon.png'),
+
+  [chains.arbitrumNova.id]: require('/assets/networks/arbitrumNova.svg'),
+  [chains.arbitrumOne.id]: require('/assets/networks/arbitrumOne.svg'),
+  [chains.avalanche.id]: require('/assets/networks/avalanche.svg'),
+  [chains.bsc.id]: require('/assets/networks/bsc.svg'),
+  [chains.celo.id]: require('/assets/networks/celo.svg'),
+  [chains.fantom.id]: require('/assets/networks/fantom.svg'),
+  [chains.gnosis.id]: require('/assets/networks/gnosis.svg'),
+  [chains.harmonyOne.id]: require('/assets/networks/harmonyOne.svg'),
+  [chains.mainnet.id]: require('/assets/networks/mainnet.svg'),
+  [chains.mantle.id]: require('/assets/networks/mantle.svg'),
+  [chains.metis.id]: require('/assets/networks/metis.svg'),
+  [chains.moonbeam.id]: require('/assets/networks/moonbeam.svg'),
+  [chains.opBNB.id]: require('/assets/networks/opBNB.svg'),
+  [chains.optimism.id]: require('/assets/networks/optimism.svg'),
+  [chains.polygon.id]: require('/assets/networks/polygon.svg'),
+  [chains.polygonZkEvm.id]: require('/assets/networks/polygonZkEvm.svg'),
+  [chains.scroll.id]: require('/assets/networks/scroll.png'),
+  [chains.zkSyncEra.id]: require('/assets/networks/zkSyncEra.svg'),
+}
