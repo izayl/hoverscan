c10c3db734b05407f17a710e439cdd71361e556f
izayl <izayl@163.com>
2023-10-16 22:59:32 +0800

    fix: webpack & parcel path-resolution

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,50 @@
-import type { StorybookConfig } from "@storybook/react-webpack5"
+import type { StorybookConfig } from '@storybook/react-webpack5'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+type Options = {
+  schemes: string[]
+}
+class AnySchemeUriPlugin {
+  constructor(public options: Options) {}
+
+  apply(compiler) {
+    compiler.hooks.compilation.tap(
+      'AnySchemeUriPlugin',
+      (compilation, { normalModuleFactory }) => {
+        Array.from(this.options.schemes).forEach(scheme => {
+          normalModuleFactory.hooks.resolveForScheme
+            .for(scheme)
+            .tap('AnySchemeUriPlugin', resourceData => {
+              const uri = resourceData.resource.replace(
+                `${scheme}:~assets`,
+                `file://${path.resolve(__dirname, '../assets')}`
+              )
+
+              const url = new URL(uri)
+              const path_ = fileURLToPath(url)
+              const query = url.search
+              const fragment = url.hash
+
+              resourceData.path = path_
+              resourceData.query = query
+              resourceData.fragment = fragment
+              resourceData.resource = path_ + query + fragment
+
+              return true
+            })
+        })
+      }
+    )
+  }
+}
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.story.@(js|jsx|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.story.@(js|jsx|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
     {
       name: '@storybook/addon-styling',
       options: {
@@ -17,11 +55,16 @@ const config: StorybookConfig = {
     },
   ],
   framework: {
-    name: "@storybook/react-webpack5",
+    name: '@storybook/react-webpack5',
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: 'tag',
+  },
+  webpack: async config => {
+    if (!config.plugins) config.plugins = []
+    config.plugins.push(new AnySchemeUriPlugin({ schemes: ['data-base64'] }))
+    return config
   },
   webpackFinal: async config => {
     if (config.resolve) {
